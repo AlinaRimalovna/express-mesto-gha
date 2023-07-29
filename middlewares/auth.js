d212cd64ec70b2041995b8d26dc807941542c536
Alina Kozhevnikova <alina_nr@mail.ru>
2023-07-29 17:55:08 +0400

    проба3

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const Unauthorized = require('../errors/Unauthorized');
 
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
@@ -6,9 +7,9 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    if (err.name === 'Unauthorized') {
+      next(new Unauthorized('Необходима авторизация'));
+    }
   }
   req.user = payload;
   return next();
