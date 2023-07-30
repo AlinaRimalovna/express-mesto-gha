71cb446238887da837b45c7315d690394acc7bdd
Alina Kozhevnikova <alina_nr@mail.ru>
2023-07-30 15:17:25 +0400

    14 работа на проверку

--- controllers/cards.js
+++ controllers/cards.js
@@ -28,10 +28,13 @@ module.exports.deleteCard = (req, res, next) => {
   Card.findById(req.params.cardId)
     .orFail(new NotFoundError('Карточка по данному Id не найдена'))
     .then((card) => {
+      // console.log(String(card.owner));
+      // console.log('1523');
+      // console.log(req.user._id);
       if (String(card.owner) === String(req.user._id)) {
         Card.deleteOne(card)
-          .orFail(new Forbidden('Недостаточно прав для удаления карточки'))
-          .then((deleteCard) => res.status(200).send(deleteCard))
+        // Card.findByIdAndRemove(req.params.cardId)
+          .then((deleteCard) => res.status(SUCCESS_CODE).send(deleteCard))
           .catch((err) => {
             if (err.name === 'CastError') {
               next(new CastError('Переданы некорректные данные при удалении карточки'));
@@ -42,6 +45,7 @@ module.exports.deleteCard = (req, res, next) => {
             next(err);
           });
       }
+      next(new Forbidden('Недостаточно прав для удаления карточки'));
     })
     .catch(next);
 };
