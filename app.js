d591b84adf0a9fcec8dddeba030b48a67131bff8
Alina Kozhevnikova <alina_nr@mail.ru>
2023-07-30 12:03:54 +0400

    корректировка2

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 // app.post('/signup', createUser);
 app.post('/signin', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().pattern(/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{1,6}\b([-a-zA-Z0-9\-._~:/?#[\]@!$&'()*+,:=]*)$/),
+    email: Joi.string().required(),
     password: Joi.string().required(),
   }),
 }), login);
@@ -29,8 +29,8 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{1,6}\b([-a-zA-Z0-9\-._~:/?#[\]@!$&'()*+,:=]*)$/),
-    email: Joi.string().required().pattern(/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{1,6}\b([-a-zA-Z0-9\-._~:/?#[\]@!$&'()*+,:=]*)$/),
+    avatar: Joi.string().pattern(/^https?:\/\/(www\.)?[-a-zA-Z0-9-._~:/?#\\[\\]@!\\$&'()*+,;=]*\\.(com|net|org|ru|png)(#.+)?$/),
+    email: Joi.string().required(),
     password: Joi.string().required(),
   }),
 }), createUser);
